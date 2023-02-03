a84307e93c956c458dc04a00def718f7bd71a0b6
andre <andre1048576@gmail.com>
2023-02-03 12:07:52 -0500

    Connected mongodb to the student file (currently hard coded ports)

--- server/app/models/student.py
+++ server/app/models/student.py
@@ -1,29 +1,30 @@
 from flask import request
 from flask_restful import Resource
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
-fakeDatabase = {
-    1:{'name':'jack'},
-    2:{'name':'john'},
-    3:{'name':'james'},
-}
+client = MongoClient("127.0.0.1",27017)
+mydb = client["fruits"]
+mycol = mydb["fruits"]
 
 class Students(Resource):
     def get(self):
-        return fakeDatabase
+        return list(mycol.find())
 
     def post(self, name):
-        itemId = len(fakeDatabase.keys()) + 1
-        fakeDatabase[itemId] = {'name':name}
-        return fakeDatabase
+        mycol.insert_one({'name':name})
+        return self.get()
 
+#why do we even need this
+#i mean i implemented it but still why
 class Student(Resource):
     def get(self, id):
-        return fakeDatabase[id]
+        return list(mycol.find({"_id" : ObjectId(id)}))
     
     def put(self, id, name):
-        fakeDatabase[id]['name'] = name
-        return fakeDatabase
+        mycol.insert_one({"_id" : ObjectId(id),'name':name})
+        return list(mycol.find())
     
     def delete(self, id):
-        del fakeDatabase[id]
-        return  fakeDatabase
+        mycol.delete_one({"_id" : ObjectId(id)})
+        return list(mycol.find())
