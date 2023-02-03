0b81c8a3df8a9d77ecfb8ef547814c42542d25a1
Laith-Grira <lgrira12@gmail.com>
2023-02-03 17:41:01 -0500

    add the enw route to main

--- server/main.py
+++ server/main.py
@@ -1,8 +1,12 @@
 from flask_restful import Api
 from routes.student import StudentAPI, StudentsAPI
+from routes.importStudents import ImportStudentsAPI
 
 
 def initialize_routes(api: Api):
-    # User routes
+    # Student routes
     api.add_resource(StudentAPI, '/student/<int:id>')
     api.add_resource(StudentsAPI, '/student')
+
+    # Import students routes
+    api.add_resource(ImportStudentsAPI, '/import-excel')
