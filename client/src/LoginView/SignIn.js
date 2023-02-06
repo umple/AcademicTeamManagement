e99cf8aa1a27fb70803dbf0049b19b781224996d
Robert <rbasi008@uottawa.ca>
2023-02-06 13:45:28 -0500

    fixed client issue

--- client/src/LoginView/SignIn.js
+++ client/src/LoginView/SignIn.js
@@ -47,7 +47,7 @@ export default function SignIn() {
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Box component="form" noValidate sx={{ mt: 1 }}>
   
             <Link
               type="submit"
