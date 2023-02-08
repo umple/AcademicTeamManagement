beda16a8bc0f87edd7c977a39361c8c98b3c402e
Laith-Grira <lgrira12@gmail.com>
2023-02-08 10:57:14 -0500

    clean hashtag symples

--- server/app/models/importStudents.py
+++ server/app/models/importStudents.py
@@ -1,6 +1,6 @@
 from flask import request
 from flask_restful import Resource
-from app.utils.data_conversion import handle_special_characters
+from app.utils.data_conversion import handle_special_characters, remove_hashtage_starting_characters
 import pandas as pd
 
 class ImportStudents(Resource):
@@ -12,7 +12,7 @@ class ImportStudents(Resource):
             file_extension = file.filename.rsplit(".", 1)[1]
             if file_extension == "xlsx":
                 data = pd.read_excel(file,na_values=["N/A", "na", "--","NaN", " "])
-                return handle_special_characters(data.to_json(orient="records"))
+                return remove_hashtage_starting_characters(data.to_json(orient="records"))
             elif file_extension == "csv":
                 data = pd.read_csv(file,na_values=["N/A", "na", "--","NaN", " "])
                 return data.to_json(orient="records")
--- server/app/utils/data_conversion.py
+++ server/app/utils/data_conversion.py
@@ -15,10 +15,26 @@ def handle_special_characters(students_list):
         new_obj = {}
         for key, value in json_obj.items():
             if isinstance(value, str) and re.search(copyright_char_regex, value):
-                print(value)
                 value = re.sub(copyright_char_regex, "é", value)
                 Index = value.index('é') - 1
                 value = value[:Index] + value[Index + 1:]
+            new_obj[key] = value
+        new_list.append(new_obj)
+    return json.dumps(new_list)
+
+'''
+    remove the hashtage symbole from the json values that start 
+    with it 
+'''
+def remove_hashtage_starting_characters(students_list):
+    students_list = json.loads(students_list)
+    hashtage_char_regex = r"#"
+    new_list = []
+    for json_obj in students_list:
+        new_obj = {}
+        for key, value in json_obj.items():
+            if isinstance(value, str) and re.search(hashtage_char_regex, value) and value.index(hashtage_char_regex)==0:
+                value = value[1:]
             new_obj[key] = value
         new_list.append(new_obj)
     return json.dumps(new_list)
