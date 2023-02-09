200c310331f376e1d690e481974369bc69c44696
Laith-Grira <lgrira12@gmail.com>
2023-02-09 15:33:42 -0500

    add redirection to home page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import ProjectTable from './ProjectListingView/Components/ProjectTable/ProjectTa
 import ImportStudents from './ImportStudentsView/ImportStudents';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignIn from './LoginView/SignIn';
+import HomePage from './HomePageView/HomePage';
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
             <ResponsiveAppBar></ResponsiveAppBar>
           </div>
           <Routes>
-            <Route path='/' element={<SignIn/>}></Route>
+            <Route path='/' element={<HomePage/>}></Route>
+            <Route path='/login' element={<SignIn/>}></Route>
             <Route exact path='/Projects' element={<ProjectTable/>}></Route>
             <Route path='/ImportStudents' element={<ImportStudents/>}></Route>
           </Routes >
---
+++ client/src/HomePageView/HomePage.js
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return(
+        <div className="homeContainer">
+            <h1>Academic Team Managment Website</h1>
+        </div>
+    )
+}
+
+export default HomePage
