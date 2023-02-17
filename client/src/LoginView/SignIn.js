3e407ff8d41fdffc59c9299527ce956a97a82394
Laith-Grira <lgrira12@gmail.com>
2023-02-17 00:11:42 -0500

    update login page

--- client/src/LoginView/SignIn.js
+++ client/src/LoginView/SignIn.js
@@ -1,37 +1,15 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
+import { Avatar, Button, Link, Box, Typography, Container, CssBaseline } from '@mui/material';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
 const theme = createTheme();
 
 export default function SignIn() {
   return (
     <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
+      <Container component="main" maxWidth="sm">
         <CssBaseline />
         <Box
           sx={{
@@ -45,7 +23,7 @@ export default function SignIn() {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign in
+            Choose Your Institution to Login
           </Typography>
           <Box component="form" noValidate sx={{ mt: 1 }}>
   
@@ -56,7 +34,9 @@ export default function SignIn() {
               href ="http://localhost:5000/login"
               sx={{ mt: 3, mb: 2 }}
             >
-              Sign In
+              <Button size='large' variant="outlined" startIcon={<AccountBalanceIcon />}>
+                University of Ottawa
+              </Button>
             </Link>
    
           </Box>
