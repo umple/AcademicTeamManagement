cf5ec5296cd5ca57a410e90719dd2d717d4a2d84
Robert <rbasi008@uottawa.ca>
2023-03-09 11:32:30 -0500

    refactoring auth

--- server/main.py
+++ server/main.py
@@ -1,12 +1,16 @@
 from flask_restful import Api
 from routes.student import StudentAPI, StudentsAPI
 from routes.importStudents import ImportStudentsAPI
+from security.auth import AuthenticationAPI
 
 
 def initialize_routes(api: Api):
     # Student routes
-    api.add_resource(StudentAPI, '/api/student/<int:id>')
+    # api.add_resource(StudentAPI, '/api/student/<int:id>')
     api.add_resource(StudentsAPI, '/api/student')
 
     # Import students routes
     api.add_resource(ImportStudentsAPI, '/api/import-excel')
+
+    # Import security
+    api.add_resource(AuthenticationAPI, '/api/login')
---
+++ server/security/auth.py
@@ -0,0 +1,19 @@
+
+from flask import Flask, redirect, session
+from requests_oauthlib import OAuth2Session
+import os
+from app.utils.decryption_manager import getDecryptedSecret
+
+class AuthenticationAPI(Resource):
+    REDIRECT_URI = "http://localhost:%s"%os.getenv("REACT")
+    def __init__(self):
+        self.AUTHORITY = getDecryptedSecret("AUTHORITY")
+        self.CLIENT_ID = getDecryptedSecret("CLIENT_ID")
+        self.CLIENT_SECRET = getDecryptedSecret("CLIENT_SECRET")
+        super(AuthenticationAPI, self).__init__()
+
+    def login(self):
+        self.oauth = OAuth2Session(self.CLIENT_ID, redirect_uri= self.REDIRECT_URI, scope=["openid", "profile"])
+        authorization_url, state = self.oauth.authorization_url(f"{self.AUTHORITY}/oauth2/v2.0/authorize")
+        session["state"] = state
+        return redirect(authorization_url)
