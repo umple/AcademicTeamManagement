833c5974631253bbceb9502705775ac8320ce66a
Robert <rbasi008@uottawa.ca>
2023-03-09 11:34:50 -0500

    resolving conflicts

--- server/app/models/student.py
+++ server/app/models/student.py
@@ -1,29 +1,29 @@
-# from flask_restful import Resource
-# from pymongo import MongoClient
-# from bson.objectid import ObjectId
-# import os
+from flask_restful import Resource
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import os
 
-# # client = MongoClient("mongodb://localhost", os.getEnv("MONGO"))
-# #currently hard coded database and collection called fruits
-# # mydb = client["fruits"]
-# # mycol = mydb["fruits"]
+client = MongoClient("mongodb://localhost", os.getenv("MONGO"))
+# currently hard coded database and collection called fruits
+mydb = client["fruits"]
+mycol = mydb["fruits"]
 
-# class Students(Resource):
-#     def get(self):
-#         return list(mycol.find())
+class Students(Resource):
+    def get(self):
+        return list(mycol.find())
 
-#     def post(self, name):
-#         mycol.insert_one({'name':name})
-#         return self.get()
+    def post(self, name):
+        mycol.insert_one({'name':name})
+        return self.get()
         
-# class Student(Resource):
-#     def get(self, id):
-#         return list(mycol.find({"_id" : ObjectId(id)}))
+class Student(Resource):
+    def get(self, id):
+        return list(mycol.find({"_id" : ObjectId(id)}))
     
-#     def put(self, id, name):
-#         mycol.update_one({"_id" : ObjectId(id)},{'name':name})
-#         return list(mycol.find())
+    def put(self, id, name):
+        mycol.update_one({"_id" : ObjectId(id)},{'name':name})
+        return list(mycol.find())
     
-#     def delete(self, id):
-#         mycol.delete_one({"_id" : ObjectId(id)})
-#         return list(mycol.find())
+    def delete(self, id):
+        mycol.delete_one({"_id" : ObjectId(id)})
+        return list(mycol.find())
