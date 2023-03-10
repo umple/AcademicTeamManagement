0ecdafae5d416124527698924da17d93198bc9b6
Caroline Tippins <carotippins@hotmail.com>
2023-03-09 19:53:22 -0500

    Reverted change made while testing deployment

--- client/src/StaticComponents/NavBar/ResponsiveAppBar.js
+++ client/src/StaticComponents/NavBar/ResponsiveAppBar.js
@@ -10,7 +10,7 @@ const pages = {
   page1: {key: 'Home', value:'/'},
   page2: {key: 'Projects', value:'/Projects'},
   page3: {key:'Groups', value:'/GroupView'},
-  page4: {key:'Import Change', value:'/ImportStudents'},
+  page4: {key:'Import Students', value:'/ImportStudents'},
   page5: {key: 'Students', value:'/Students'}
 
 };
