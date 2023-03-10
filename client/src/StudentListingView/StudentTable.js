53444c0fc88730ed226d4624f86f418cda4a3c0e
Caroline Tippins <carotippins@hotmail.com>
2023-03-09 19:45:33 -0500

    created student list

---
+++ client/src/StudentListingView/StudentTable.js
@@ -0,0 +1,321 @@
+import React, { useCallback, useState, useMemo } from 'react';
+import MaterialReactTable from 'material-react-table';
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  Stack,
+  TextField,
+  Tooltip,
+  Typography,
+  Grid,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  FormControlLabel,
+  Checkbox,
+  FormLabel,
+  FormGroup,
+} from '@mui/material';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import { ExportToCsv } from 'export-to-csv';
+import { Delete, Edit, Help } from '@mui/icons-material';
+import { createTheme } from '@mui/material/styles';
+
+// Mock data for table
+const data = [
+  {
+    orgDefinedId: '11111',
+    username: 'UBIPL061',
+    team: 'Team 1',
+    lastName: 'Doe',
+    firstName: 'John',
+    section: 'SEG4910W00',
+    notes: 'Contacted about open market'
+  },
+  {
+    orgDefinedId: '11122',
+    username: 'UBIPL062',
+    team: 'Team 2',
+    lastName: 'Brown',
+    firstName: 'Catherine',
+    section: 'SEG4910W00',
+    notes: 'Failed a pre-requisite'
+  },
+  {
+    orgDefinedId: '11122',
+    username: 'UBIPL063',
+    team: 'Team 3',
+    lastName: 'Smith',
+    firstName: 'Jane',
+    section: 'SEG4910W01',
+    notes: 'Only speaks French'
+  },
+  {
+    orgDefinedId: '44444',
+    username: 'UBIPL064',
+    team: 'unassigned',
+    lastName: 'Doe',
+    firstName: 'John',
+    section: 'SEG4910W00',
+    notes: ''
+  },
+  {
+    orgDefinedId: '11122',
+    username: 'UBIPL065',
+    team: 'unassigned',
+    lastName: 'James',
+    firstName: 'Andrew',
+    section: 'SEG4910W00',
+    notes: 'Wants to work on a project with a friend'
+  },
+  {
+    orgDefinedId: '11122',
+    username: 'UBIPL062',
+    team: 'Team 2',
+    lastName: 'Biden',
+    firstName: 'Joe',
+    section: 'SEG4910W01',
+    notes: ''
+  },
+];
+
+
+
+const StudentTable = () => {
+  // Columns for table
+  const columns = useMemo(
+    () => [
+          {
+            accessorKey: 'orgDefinedId',
+            header: 'ID',
+          },
+          {
+            accessorKey: 'username',
+            header: 'Username',
+          },
+          {
+            accessorKey: 'firstName',
+            header: 'First Name',
+          },
+          {
+            accessorKey: 'lastName',
+            header: 'Last Name',
+          },
+          {
+            accessorKey: 'section',
+            header: 'Section',
+          },
+          {
+            accessorKey: 'notes',
+            header: 'Notes',
+          },
+        ],
+    [],
+  );
+
+
+  // For the create profile modal
+  const [createModalOpen, setCreateModalOpen] = useState(false);
+  const [tableData, setTableData] = useState(() => data);
+  const [validationErrors, setValidationErrors] = useState({});
+
+  const handleCreateNewRow = (values) => {
+    tableData.push(values);
+    setTableData([...tableData]);
+    console.log(tableData)
+    console.log(columns)
+  };
+
+  const handleSaveRowEdits = async ({ exitEditingMode, row, values }) => {
+    if (!Object.keys(validationErrors).length) {
+      tableData[row.index] = values;
+      setTableData([...tableData]);
+      exitEditingMode();
+    }
+  };
+
+  const handleCancelRowEdits = () => {
+    setValidationErrors({});
+  };
+
+
+  // For the model to view student applications
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const style = {
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+  
+  // To delete the row
+  const handleDeleteRow = useCallback(
+    (row) => {
+      if (
+        !window.confirm(`Are you sure you want to delete ${row.getValue('firstName')}`)
+      ) {
+        return;
+      }
+      //send api delete request here, then refetch or update local table data for re-render
+      tableData.splice(row.index, 1);
+      setTableData([...tableData]);
+    },
+    [tableData],
+  );
+
+  // For exporting the table data
+  const csvOptions = {
+    fieldSeparator: ',',
+    quoteStrings: '"',
+    decimalSeparator: '.',
+    showLabels: true,
+    useBom: true,
+    useKeysAsHeaders: false,
+    headers: columns.map((c) => c.header),
+  };
+
+  const csvExporter = new ExportToCsv(csvOptions);
+
+  const handleExportData = () => {
+    csvExporter.generateCsv(data);
+  };
+  
+   
+  return(
+  <Box sx={{ p: 2 }}>
+    <MaterialReactTable
+        displayColumnDefOptions={{
+          'mrt-row-actions': {
+            muiTableHeadCellProps: {
+              align: 'center',
+            },
+            size: 120,
+          },
+        }}
+        enablePagination={false}
+        columns={columns}
+        data={tableData}
+        editingMode="modal"
+        enableColumnOrdering
+        enableColumnResizing
+        columnResizeMode="onChange" //default is "onEnd"
+        defaultColumn={{
+          minSize: 100,
+          size: 150, //default size is usually 180
+        }}
+        enableEditing
+        initialState={{ showColumnFilters: false, density: 'compact' }}
+        onEditingRowSave={handleSaveRowEdits}
+        onEditingRowCancel={handleCancelRowEdits}
+        renderRowActions={({ row, table }) => (
+          <Box sx={{ display: 'flex', gap: '1rem' }}>
+            <Tooltip arrow placement="left" title="Edit">
+              <IconButton onClick={() => table.setEditingRow(row)}>
+                <Edit />
+              </IconButton>
+            </Tooltip>
+            <Tooltip arrow placement="right" title="Delete">
+              <IconButton color="error" onClick={() => handleDeleteRow(row)}>
+                <Delete />
+              </IconButton>
+            </Tooltip>
+          </Box>
+        )}
+        renderTopToolbarCustomActions={() => (
+          <Box sx={{ display: 'flex', gap: '1rem', p: '0.5rem', flexWrap: 'wrap' }}>
+            <Button
+              color="success"
+              onClick={() => setCreateModalOpen(true)}
+              variant="contained"
+            >
+              Create New Student
+            </Button>
+            <Button
+            color="primary"
+            //export all data that is currently in the table (ignore pagination, sorting, filtering, etc.)
+            onClick={handleExportData}
+            startIcon={<FileDownloadIcon />}
+            variant="contained"
+            >
+              Export All Data
+            </Button>
+          </Box>
+        )}
+      />
+      <CreateNewStudentModal
+        columns={columns}
+        open={createModalOpen}
+        onClose={() => setCreateModalOpen(false)}
+        onSubmit={handleCreateNewRow}
+      />
+  </Box>
+  );
+};
+
+//Modal to create new student
+export const CreateNewStudentModal = ({ open, columns, onClose, onSubmit }) => {
+  const [values, setValues] = useState(() =>
+    columns.reduce((acc, column) => {
+      acc[column.accessorKey ?? ''] = '';
+      return acc;
+    }, {}),
+  );
+
+  const handleSubmit = () => {
+    //put your validation logic here
+    onSubmit(values);
+    onClose();
+  };
+
+  return (
+    <Dialog open={open}>
+      <DialogTitle textAlign="center">Create New Student</DialogTitle>
+      <DialogContent>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <Stack
+            sx={{
+              width: '100%',
+              minWidth: { xs: '300px', sm: '360px', md: '400px' },
+              gap: '1.5rem',
+            }}
+          >
+            {columns.map((column) => (
+              <TextField
+                key={column.accessorKey}
+                label={column.header}
+                name={column.accessorKey}
+                onChange={(e) =>
+                  setValues({ ...values, [e.target.name]: e.target.value })
+                }
+              />
+            ))}
+          </Stack>
+        </form>
+      </DialogContent>
+      <DialogActions sx={{ p: '1.25rem' }}>
+        <Button onClick={onClose}>Cancel</Button>
+        <Button color="secondary" onClick={handleSubmit} variant="contained">
+          Create New Student
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default StudentTable;
