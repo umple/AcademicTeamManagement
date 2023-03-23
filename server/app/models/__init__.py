deb62601f423d578ad34e98478a3cb2d6b3a6ef3
Laith-Grira <lgrira12@gmail.com>
2023-03-23 11:11:11 -0400

    update the db config

--- server/app/models/__init__.py
+++ server/app/models/__init__.py
@@ -1,4 +1,12 @@
+import os
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient(
+    host=os.getenv("MONGODB_HOST"),
+    port=int(os.getenv("MONGO")),
+    username=os.getenv("MONGODB_INITDB_ROOT_USERNAME"),
+    password=os.getenv("MONGODB_INITDB_ROOT_PASSWORD"),
+    authSource="admin"
+)
+
 db = client["AcademicTeamManagementDB"]
