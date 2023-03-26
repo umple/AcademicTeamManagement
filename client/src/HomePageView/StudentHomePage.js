50afba448d0eaa1da15b6fa9b4b96650d82588c7
Caroline Tippins <carotippins@hotmail.com>
2023-03-26 17:37:21 -0400

    Created basic homepage for students

---
+++ client/src/HomePageView/StudentHomePage.js
@@ -0,0 +1,51 @@
+// Imports
+import {
+    makeStyles,
+    Grid,
+    Typography,
+  } from "@material-ui/core";
+  
+  // Styles
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+      padding: theme.spacing(4),
+      backgroundColor: '#f5f5f5',
+    },
+    title: {
+      fontSize: "4rem",
+      fontWeight: 700,
+      color: '#3f51b5',
+    },
+    column1: {
+      paddingLeft: 50,
+    },
+    welcomeText: {
+      fontSize: '2rem',
+      fontWeight: 500,
+      color: '#616161',
+    },
+  }));
+  
+  // StudentHomePage Component
+  function StudentHomePage() {
+    const classes = useStyles();
+  
+    return (
+      <div className={classes.root}>
+        <Grid container spacing={4} justifyContent="center" alignItems="center">
+          <Grid item sm={6} className={classes.column1}>
+            <Typography variant="h3" className={classes.title} gutterBottom>
+              Academic Team Management
+            </Typography>
+            <Typography variant="h2" className={classes.welcomeText} gutterBottom>
+              Welcome Student!
+            </Typography>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+  
+  export default StudentHomePage;
+  
