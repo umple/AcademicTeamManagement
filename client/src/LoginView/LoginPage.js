699460b50ee43874679da125e651bbf8460fe2dd
Laith-Grira <lgrira12@gmail.com>
2023-04-03 17:27:31 -0400

    current working version

--- client/src/LoginView/LoginPage.js
+++ client/src/LoginView/LoginPage.js
@@ -3,30 +3,10 @@ import { Avatar, Button, Link, Box, Typography, Container, CssBaseline } from '@
 import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import axios from 'axios';
-import { useState } from 'react';
 
 const theme = createTheme();
 
 export default function LoginPage() {
-
-  const [error, setError] = useState(null);
-
-  const handleLogin = async () => {
-    try {
-      // Call the API to initiate the OAuth2 flow
-      const response = await axios({
-        method: 'get',
-        url:`http://localhost:${process.env.REACT_APP_FLASK}/api/login`
-      })
-
-      window.location.replace(response.data);
-    } catch (error) {
-      setError('Failed to initiate login');
-    }
-  };
-
-
   return (
     <ThemeProvider theme={theme}>
       <Container component="main" maxWidth="sm">
@@ -47,13 +27,17 @@ export default function LoginPage() {
           </Typography>
 
           <Box component="form" noValidate sx={{ mt: 1 }}>
-              <Button 
-                size='large' 
-                variant="outlined" 
-                startIcon={<AccountBalanceIcon />}
-                onclick={handleLogin()}
-                >University of Ottawa
+            <Link
+              href={`http://localhost:${process.env.REACT_APP_FLASK}/api/login`}
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              <Button size='large' variant="outlined" startIcon={<AccountBalanceIcon />}>
+                University of Ottawa
               </Button>
+            </Link>
           </Box>
         </Box>
       </Container>
--- server/security/auth.py
+++ server/security/auth.py
@@ -17,19 +17,40 @@ class AuthenticationAPI(Resource):
         self.CLIENT_SECRET = getDecryptedSecret("CLIENT_SECRET")
         super(AuthenticationAPI, self).__init__()
 
+    def obtain_accesstoken(self, TENANT_NAME, CLIENT_ID, CLIENT_SECRET, resource):
+        """
+        This function is used to get an access token to MS Graph.
+
+        :param TENANT_NAME: The name of the Azure tenant
+        :param CLIENT_ID: The ID of the registered Azure AD application
+        :param CLIENT_SECRET: Secret of the registered Azure AD application
+        :param resource: The resource to get an access token for
+        :return: The access token
+        """
+        auth_context = adal.AuthenticationContext('https://login.microsoftonline.com/' + TENANT_NAME)
+        token = auth_context.acquire_token_with_client_credentials(
+            resource=resource, client_id=CLIENT_ID,
+            client_secret=CLIENT_SECRET)
+        return token
+
     def get_access_token_from_azure_ad(self):
         RESOURCE = "https://graph.microsoft.com"
-        context = adal.AuthenticationContext(self.AUTHORITY, api_version=None)
-        token = context.acquire_token_with_client_credentials(RESOURCE, self.CLIENT_ID, self.CLIENT_SECRET)
+        TENANT_NAME = "AcademicTeamManagement"
+        #token = context.acquire_token_with_client_credentials(TENANT_NAME, self.CLIENT_ID, self.CLIENT_SECRET, RESOURCE)
+        token = self.obtain_accesstoken(TENANT_NAME, self.CLIENT_ID, self.CLIENT_SECRET, RESOURCE)
+        print("--------Access Token---------")
+        print(token)
         return token.get("accessToken")
     
     def get_user_id_from_azure_ad(self):
-        token =  self.get_access_token_from_azure_ad()
+        token2 =  self.get_access_token_from_azure_ad()
+        token = "eyJ0eXAiOiJKV1QiLCJub25jZSI6IlptZWdJT1Z2LTBmWlBQcERaaEx6cWo3UURhbElJRllmODI0QkxKbEE4UGMiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiIwMDAwMDAwMy0wMDAwLTAwMDAtYzAwMC0wMDAwMDAwMDAwMDAiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC9kNDFmZGFiMS03ZTE1LTRjZmQtYjVmYS03MjAwZTU0ZGViNmIvIiwiaWF0IjoxNjgwNTU1MTIxLCJuYmYiOjE2ODA1NTUxMjEsImV4cCI6MTY4MDY0MTgyMSwiYWNjdCI6MCwiYWNyIjoiMSIsImFpbyI6IkFYUUFpLzhUQUFBQUpMa1NDM3ZTeWtKeTBHclROV2cvQ1hJb1NGMGJ0Z01DTEhUMWlmaUNobWcra0NVSmVLUk5qc2hKSkh1NFA3NEJ6c1dCelR4S0ZvcUorT1gyVTU4NUpTZC8rN1JUVFczZmhDQ056dEJjeTJBV3BjNFNpUjlUYTdvb2tQUVAwenp5WTYyWU5oZjNvQVBoQ1haeStQbmVhZz09IiwiYW1yIjpbInB3ZCIsIm1mYSJdLCJhcHBfZGlzcGxheW5hbWUiOiJHcmFwaCBFeHBsb3JlciIsImFwcGlkIjoiZGU4YmM4YjUtZDlmOS00OGIxLWE4YWQtYjc0OGRhNzI1MDY0IiwiYXBwaWRhY3IiOiIwIiwiZmFtaWx5X25hbWUiOiJHcmlyYSIsImdpdmVuX25hbWUiOiJMYWl0aCIsImlkdHlwIjoidXNlciIsImlwYWRkciI6IjEzNy4xMjIuNjQuMjA2IiwibmFtZSI6IkxhaXRoIEdyaXJhIiwib2lkIjoiOWM4M2FmYzMtOWU1NS00M2RkLThlMzktNjM2NjI4ZDMzYWQ0Iiwib25wcmVtX3NpZCI6IlMtMS01LTIxLTI4MTM2MDM5MTUtMTQ5Nzk1OTU3Ny0xMDE1NzE3MzExLTEwMjk2NTIiLCJwbGF0ZiI6IjMiLCJwdWlkIjoiMTAwMzIwMDAzNzU1MDI1MyIsInJoIjoiMC5BWFlBc2RvZjFCVi1fVXkxLW5JQTVVM3Jhd01BQUFBQUFBQUF3QUFBQUFBQUFBQjJBQkUuIiwic2NwIjoiQVBJQ29ubmVjdG9ycy5SZWFkLkFsbCBBUElDb25uZWN0b3JzLlJlYWRXcml0ZS5BbGwgQ2FsZW5kYXJzLlJlYWRXcml0ZSBDb250YWN0cy5SZWFkV3JpdGUgRGV2aWNlLkNvbW1hbmQgRGV2aWNlLlJlYWQgRGV2aWNlLlJlYWQuQWxsIERldmljZUxvY2FsQ3JlZGVudGlhbC5SZWFkLkFsbCBEZXZpY2VNYW5hZ2VtZW50QXBwcy5SZWFkLkFsbCBEZXZpY2VNYW5hZ2VtZW50QXBwcy5SZWFkV3JpdGUuQWxsIERldmljZU1hbmFnZW1lbnRDb25maWd1cmF0aW9uLlJlYWQuQWxsIERldmljZU1hbmFnZW1lbnRDb25maWd1cmF0aW9uLlJlYWRXcml0ZS5BbGwgRGV2aWNlTWFuYWdlbWVudE1hbmFnZWREZXZpY2VzLlByaXZpbGVnZWRPcGVyYXRpb25zLkFsbCBEZXZpY2VNYW5hZ2VtZW50TWFuYWdlZERldmljZXMuUmVhZC5BbGwgRGV2aWNlTWFuYWdlbWVudE1hbmFnZWREZXZpY2VzLlJlYWRXcml0ZS5BbGwgRGV2aWNlTWFuYWdlbWVudFJCQUMuUmVhZC5BbGwgRGV2aWNlTWFuYWdlbWVudFJCQUMuUmVhZFdyaXRlLkFsbCBEZXZpY2VNYW5hZ2VtZW50U2VydmljZUNvbmZpZy5SZWFkLkFsbCBEZXZpY2VNYW5hZ2VtZW50U2VydmljZUNvbmZpZy5SZWFkV3JpdGUuQWxsIERpcmVjdG9yeS5SZWFkLkFsbCBGaWxlcy5SZWFkV3JpdGUuQWxsIEdyb3VwLlJlYWQuQWxsIE1haWwuUmVhZFdyaXRlIE5vdGVzLlJlYWRXcml0ZS5BbGwgb3BlbmlkIFBlb3BsZS5SZWFkIFBlb3BsZS5SZWFkLkFsbCBQb2xpY3kuUmVhZC5BbGwgUG9saWN5LlJlYWRXcml0ZS5EZXZpY2VDb25maWd1cmF0aW9uIHByb2ZpbGUgU2l0ZXMuUmVhZFdyaXRlLkFsbCBUYXNrcy5SZWFkV3JpdGUgVXNlci5SZWFkIFVzZXIuUmVhZC5BbGwgVXNlci5SZWFkQmFzaWMuQWxsIFVzZXIuUmVhZFdyaXRlIGVtYWlsIiwic2lnbmluX3N0YXRlIjpbImttc2kiXSwic3ViIjoicDZYb2ZIRWRNYTlKa3dRZWFOVmNEQWZFZVZvUWV6Z0pCUFJtTGlYMk5uMCIsInRlbmFudF9yZWdpb25fc2NvcGUiOiJOQSIsInRpZCI6ImQ0MWZkYWIxLTdlMTUtNGNmZC1iNWZhLTcyMDBlNTRkZWI2YiIsInVuaXF1ZV9uYW1lIjoibGdyaXIwNTdAdW90dGF3YS5jYSIsInVwbiI6ImxncmlyMDU3QHVvdHRhd2EuY2EiLCJ1dGkiOiJUNlZZSVdiUjFrSzEwMnVkbG9hUEFBIiwidmVyIjoiMS4wIiwid2lkcyI6WyJiNzlmYmY0ZC0zZWY5LTQ2ODktODE0My03NmIxOTRlODU1MDkiXSwieG1zX2NjIjpbIkNQMSJdLCJ4bXNfc3NtIjoiMSIsInhtc19zdCI6eyJzdWIiOiJoRERDLW9SVkZyZmdwQkxhNGJuTDhmWDd0M2UxWnMzd2ttZFlhZk54T0xNIn0sInhtc190Y2R0IjoxNTAwMTYzNzg2fQ.KzciNuzrFYjK8-DSfwmN7QEEywTsL6MPxNaT-ylfU29vrVNyM0Mlx4Rz82-GZ-6DKFFtelVlZxkYLZPYo7n7oftjNtzejyi5qEPU56fpqnWDLwluCVBIUnDUl7i6nSRxYCqJpmOAYpjMlamFdqfGtWiBlBP5ib-oy5iWw1xiHbr32pu2RbAYeEvfThBq9BKJ_tC0ta5_hg-7HRM2r7Bp82yAt2QinWPsYtJ5U52N77gJtmqM50zXA-mw9hOeg1G5FSxeVJ5z0tUnwdSDLKNeakbt7QQJvRjxU32QbCRlSRczDgvKtK6qb7Hb2poFuQA68Swn2XibZceODKXQoGlOiw"
         headers = {
-            "Authorization": f"Bearer {token}",
+            "Authorization": token,
             "Content-Type": "application/json"
         }
-        response = requests.get("https://graph.microsoft.com/v1.0/me?$select=id", headers=headers)
+        response = requests.get("https://graph.microsoft.com/v1.0/me", headers=headers)
+        #print(token2)
         print(response.json()) # the response here is returing 400
 
         if response.status_code == 200:
@@ -39,7 +60,8 @@ class AuthenticationAPI(Resource):
             return None
 
     def get_user_role_from_azure_ad(self):
-        token = self.get_access_token_from_azure_ad()  # Replace with your code to get access token
+        #token = self.get_access_token_from_azure_ad()  # Replace with your code to get access token
+        token = "eyJ0eXAiOiJKV1QiLCJub25jZSI6IlptZWdJT1Z2LTBmWlBQcERaaEx6cWo3UURhbElJRllmODI0QkxKbEE4UGMiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiIwMDAwMDAwMy0wMDAwLTAwMDAtYzAwMC0wMDAwMDAwMDAwMDAiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC9kNDFmZGFiMS03ZTE1LTRjZmQtYjVmYS03MjAwZTU0ZGViNmIvIiwiaWF0IjoxNjgwNTU1MTIxLCJuYmYiOjE2ODA1NTUxMjEsImV4cCI6MTY4MDY0MTgyMSwiYWNjdCI6MCwiYWNyIjoiMSIsImFpbyI6IkFYUUFpLzhUQUFBQUpMa1NDM3ZTeWtKeTBHclROV2cvQ1hJb1NGMGJ0Z01DTEhUMWlmaUNobWcra0NVSmVLUk5qc2hKSkh1NFA3NEJ6c1dCelR4S0ZvcUorT1gyVTU4NUpTZC8rN1JUVFczZmhDQ056dEJjeTJBV3BjNFNpUjlUYTdvb2tQUVAwenp5WTYyWU5oZjNvQVBoQ1haeStQbmVhZz09IiwiYW1yIjpbInB3ZCIsIm1mYSJdLCJhcHBfZGlzcGxheW5hbWUiOiJHcmFwaCBFeHBsb3JlciIsImFwcGlkIjoiZGU4YmM4YjUtZDlmOS00OGIxLWE4YWQtYjc0OGRhNzI1MDY0IiwiYXBwaWRhY3IiOiIwIiwiZmFtaWx5X25hbWUiOiJHcmlyYSIsImdpdmVuX25hbWUiOiJMYWl0aCIsImlkdHlwIjoidXNlciIsImlwYWRkciI6IjEzNy4xMjIuNjQuMjA2IiwibmFtZSI6IkxhaXRoIEdyaXJhIiwib2lkIjoiOWM4M2FmYzMtOWU1NS00M2RkLThlMzktNjM2NjI4ZDMzYWQ0Iiwib25wcmVtX3NpZCI6IlMtMS01LTIxLTI4MTM2MDM5MTUtMTQ5Nzk1OTU3Ny0xMDE1NzE3MzExLTEwMjk2NTIiLCJwbGF0ZiI6IjMiLCJwdWlkIjoiMTAwMzIwMDAzNzU1MDI1MyIsInJoIjoiMC5BWFlBc2RvZjFCVi1fVXkxLW5JQTVVM3Jhd01BQUFBQUFBQUF3QUFBQUFBQUFBQjJBQkUuIiwic2NwIjoiQVBJQ29ubmVjdG9ycy5SZWFkLkFsbCBBUElDb25uZWN0b3JzLlJlYWRXcml0ZS5BbGwgQ2FsZW5kYXJzLlJlYWRXcml0ZSBDb250YWN0cy5SZWFkV3JpdGUgRGV2aWNlLkNvbW1hbmQgRGV2aWNlLlJlYWQgRGV2aWNlLlJlYWQuQWxsIERldmljZUxvY2FsQ3JlZGVudGlhbC5SZWFkLkFsbCBEZXZpY2VNYW5hZ2VtZW50QXBwcy5SZWFkLkFsbCBEZXZpY2VNYW5hZ2VtZW50QXBwcy5SZWFkV3JpdGUuQWxsIERldmljZU1hbmFnZW1lbnRDb25maWd1cmF0aW9uLlJlYWQuQWxsIERldmljZU1hbmFnZW1lbnRDb25maWd1cmF0aW9uLlJlYWRXcml0ZS5BbGwgRGV2aWNlTWFuYWdlbWVudE1hbmFnZWREZXZpY2VzLlByaXZpbGVnZWRPcGVyYXRpb25zLkFsbCBEZXZpY2VNYW5hZ2VtZW50TWFuYWdlZERldmljZXMuUmVhZC5BbGwgRGV2aWNlTWFuYWdlbWVudE1hbmFnZWREZXZpY2VzLlJlYWRXcml0ZS5BbGwgRGV2aWNlTWFuYWdlbWVudFJCQUMuUmVhZC5BbGwgRGV2aWNlTWFuYWdlbWVudFJCQUMuUmVhZFdyaXRlLkFsbCBEZXZpY2VNYW5hZ2VtZW50U2VydmljZUNvbmZpZy5SZWFkLkFsbCBEZXZpY2VNYW5hZ2VtZW50U2VydmljZUNvbmZpZy5SZWFkV3JpdGUuQWxsIERpcmVjdG9yeS5SZWFkLkFsbCBGaWxlcy5SZWFkV3JpdGUuQWxsIEdyb3VwLlJlYWQuQWxsIE1haWwuUmVhZFdyaXRlIE5vdGVzLlJlYWRXcml0ZS5BbGwgb3BlbmlkIFBlb3BsZS5SZWFkIFBlb3BsZS5SZWFkLkFsbCBQb2xpY3kuUmVhZC5BbGwgUG9saWN5LlJlYWRXcml0ZS5EZXZpY2VDb25maWd1cmF0aW9uIHByb2ZpbGUgU2l0ZXMuUmVhZFdyaXRlLkFsbCBUYXNrcy5SZWFkV3JpdGUgVXNlci5SZWFkIFVzZXIuUmVhZC5BbGwgVXNlci5SZWFkQmFzaWMuQWxsIFVzZXIuUmVhZFdyaXRlIGVtYWlsIiwic2lnbmluX3N0YXRlIjpbImttc2kiXSwic3ViIjoicDZYb2ZIRWRNYTlKa3dRZWFOVmNEQWZFZVZvUWV6Z0pCUFJtTGlYMk5uMCIsInRlbmFudF9yZWdpb25fc2NvcGUiOiJOQSIsInRpZCI6ImQ0MWZkYWIxLTdlMTUtNGNmZC1iNWZhLTcyMDBlNTRkZWI2YiIsInVuaXF1ZV9uYW1lIjoibGdyaXIwNTdAdW90dGF3YS5jYSIsInVwbiI6ImxncmlyMDU3QHVvdHRhd2EuY2EiLCJ1dGkiOiJUNlZZSVdiUjFrSzEwMnVkbG9hUEFBIiwidmVyIjoiMS4wIiwid2lkcyI6WyJiNzlmYmY0ZC0zZWY5LTQ2ODktODE0My03NmIxOTRlODU1MDkiXSwieG1zX2NjIjpbIkNQMSJdLCJ4bXNfc3NtIjoiMSIsInhtc19zdCI6eyJzdWIiOiJoRERDLW9SVkZyZmdwQkxhNGJuTDhmWDd0M2UxWnMzd2ttZFlhZk54T0xNIn0sInhtc190Y2R0IjoxNTAwMTYzNzg2fQ.KzciNuzrFYjK8-DSfwmN7QEEywTsL6MPxNaT-ylfU29vrVNyM0Mlx4Rz82-GZ-6DKFFtelVlZxkYLZPYo7n7oftjNtzejyi5qEPU56fpqnWDLwluCVBIUnDUl7i6nSRxYCqJpmOAYpjMlamFdqfGtWiBlBP5ib-oy5iWw1xiHbr32pu2RbAYeEvfThBq9BKJ_tC0ta5_hg-7HRM2r7Bp82yAt2QinWPsYtJ5U52N77gJtmqM50zXA-mw9hOeg1G5FSxeVJ5z0tUnwdSDLKNeakbt7QQJvRjxU32QbCRlSRczDgvKtK6qb7Hb2poFuQA68Swn2XibZceODKXQoGlOiw"
         user_id = self.get_user_id_from_azure_ad()  # Replace with your code to get user ID
 
         # Construct the URL to retrieve the user's roles
@@ -47,7 +69,7 @@ class AuthenticationAPI(Resource):
 
         # Set the headers with the access token
         headers = {
-            "Authorization": f"Bearer {token}",
+            "Authorization": token,
             "Content-Type": "application/json"
         }
 
@@ -62,24 +84,24 @@ class AuthenticationAPI(Resource):
                 roles.append(item["appRoleDefinition"]["displayName"])
         return roles[0] if roles else None
     
-    def get_user_role(self):
-        access_token = self.get_access_token_from_azure_ad()
-        headers = {"Authorization": "Bearer " + access_token}
-        try:
-            response = requests.get("https://graph.microsoft.com/v1.0/me/memberOf", headers=headers)
-            response.raise_for_status()
-            data = response.json()
-            roles = [group["displayName"] for group in data["value"] if "Security Group" in group["@odata.type"]]
-            # Here you can write the logic to determine the user's role based on the groups they are a member of.
-            # For example, if the user is a member of a group called "Admins", return "admin". Otherwise, return "user".
-            if "Admins" in roles:
-                return "admin"
-            else:
-                return "user"
-        except HTTPError as http_err:
-            print(f"HTTP error occurred: {http_err}")
-        except Exception as err:
-            print(f"Other error occurred: {err}")
+    # def get_user_role(self):
+    #     access_token = self.get_access_token_from_azure_ad()
+    #     headers = {"Authorization": "Bearer " + access_token}
+    #     try:
+    #         response = requests.get("https://graph.microsoft.com/v1.0/me/memberOf", headers=headers)
+    #         response.raise_for_status()
+    #         data = response.json()
+    #         roles = [group["displayName"] for group in data["value"] if "Security Group" in group["@odata.type"]]
+    #         # Here you can write the logic to determine the user's role based on the groups they are a member of.
+    #         # For example, if the user is a member of a group called "Admins", return "admin". Otherwise, return "user".
+    #         if "Admins" in roles:
+    #             return "admin"
+    #         else:
+    #             return "user"
+    #     except HTTPError as http_err:
+    #         print(f"HTTP error occurred: {http_err}")
+    #     except Exception as err:
+    #         print(f"Other error occurred: {err}")
 
 
     def get(self):
@@ -87,23 +109,23 @@ class AuthenticationAPI(Resource):
         if role == "admin":
             redirect_uri = "http://localhost:%s/"%os.getenv("REACT")
         else:
-            redirect_uri = "http://localhost:%s/StudentHome"%os.getenv("REACT")
+            redirect_uri = "http://localhost:%s/"%os.getenv("REACT")
         self.oauth = OAuth2Session(self.CLIENT_ID, redirect_uri=redirect_uri, scope=["openid", "profile"])
         authorization_url, state = self.oauth.authorization_url(f"{self.AUTHORITY}/oauth2/v2.0/authorize")
         session["state"] = state
         return redirect(authorization_url)
         
-    def post(self):
-        REDIRECT_URI = "http://localhost:%s/"%os.getenv("REACT")
-        self.oauth = OAuth2Session(self.CLIENT_ID, state=session.get("state"), redirect_uri=REDIRECT_URI)
-        token = self.oauth.fetch_token(f"{self.AUTHORITY}/oauth2/v2.0/token", client_secret=self.CLIENT_SECRET, authorization_response=request.url)
+    # def post(self):
+    #     REDIRECT_URI = "http://localhost:%s/"%os.getenv("REACT")
+    #     self.oauth = OAuth2Session(self.CLIENT_ID, state=session.get("state"), redirect_uri=REDIRECT_URI)
+    #     token = self.oauth.fetch_token(f"{self.AUTHORITY}/oauth2/v2.0/token", client_secret=self.CLIENT_SECRET, authorization_response=request.url)
 
-        # Decode the ID token to extract the roles claim
-        decoded_token = jwt.decode(token['id_token'], verify=False)
-        roles = decoded_token.get('roles', [])
+    #     # Decode the ID token to extract the roles claim
+    #     decoded_token = jwt.decode(token['id_token'], verify=False)
+    #     roles = decoded_token.get('roles', [])
 
-        # Create a new token with the user type included
-        access_token = create_access_token(identity=token['accessToken'], user_claims={'roles': roles})
+    #     # Create a new token with the user type included
+    #     access_token = create_access_token(identity=token['accessToken'], user_claims={'roles': roles})
 
-        session["accessToken"] = access_token
-        return redirect("/") # Redirect to the homepage or the next page you want to display.
+    #     session["accessToken"] = access_token
+    #     return redirect("/") # Redirect to the homepage or the next page you want to display.e homepage or the next page you want to display.
