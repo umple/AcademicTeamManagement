bf5b9f0be0396eedb6e74ac6217bdc42f50258ad
Laith-Grira <lgrira12@gmail.com>
2023-04-03 12:35:20 -0400

    update login page

--- client/src/LoginView/LoginPage.js
+++ client/src/LoginView/LoginPage.js
@@ -3,10 +3,33 @@ import { Avatar, Button, Link, Box, Typography, Container, CssBaseline } from '@
 import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import axios from 'axios';
+import { useState } from 'react';
 
 const theme = createTheme();
 
 export default function LoginPage() {
+
+  const [error, setError] = useState(null);
+
+  const handleLogin = async () => {
+    setLoading(true);
+    try {
+      // Call the API to initiate the OAuth2 flow
+      const response = await axios({
+        method: 'get',
+        url:`http://localhost:${process.env.REACT_APP_FLASK}/api/login`
+      })
+
+      window.location.replace(response.data);
+    } catch (error) {
+      setError('Failed to initiate login');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+
   return (
     <ThemeProvider theme={theme}>
       <Container component="main" maxWidth="sm">
@@ -27,17 +50,13 @@ export default function LoginPage() {
           </Typography>
 
           <Box component="form" noValidate sx={{ mt: 1 }}>
-            <Link
-              href={`http://localhost:${process.env.REACT_APP_FLASK}/api/login`}
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              <Button size='large' variant="outlined" startIcon={<AccountBalanceIcon />}>
-                University of Ottawa
+              <Button 
+                size='large' 
+                variant="outlined" 
+                startIcon={<AccountBalanceIcon />}
+                onclick={handleLogin()}
+                >University of Ottawa
               </Button>
-            </Link>
           </Box>
         </Box>
       </Container>
