8a4986c5dbb0db5f6326a905ee60287b41c18dda
Laith-Grira <lgrira12@gmail.com>
2023-04-03 12:30:35 -0400

    add call to the backend from the route

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,51 @@ import StudentTable from './StudentListingView/StudentTable';
 import ImportStudents from './ImportStudentsView/ImportStudents';
 import Groups from './GroupView/GroupTable'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import LoginPage from './LoginView/LoginPage';
 import HomePage from './HomePageView/HomePage';
+import axios from 'axios';
+import StudentHomePage from './HomePageView/StudentHomePage';
 import StudentProjects from './StudentView/StudentProjects';
 import StudentGroups from './StudentView/StudentGroupTable';
 import MyGroup from './StudentView/MyGroup';
 
-function App() {
+
+const [user, setUser] = useState(null);
+
+const handleAuthentication = async () => {
+  try {
+    // Call the API to complete the OAuth2 flow
+    const response = await axios.post('http://localhost:5001/api/login', { url: window.location.href }); // proxy is not working for some reason
+    const { access_token } = response.data;
+
+    // Decode the JWT to get the user's roles
+    const decodedToken = JSON.parse(atob(access_token.split('.')[1]));
+    const roles = decodedToken.roles;
+
+    // Set the user based on their role and redirect
+    if (roles.includes('professor')) {
+      setUser({ role: 'professor' });
+      window.location.replace('/'); // professor view
+    } else if (roles.includes('student')) {
+      setUser({ role: 'student' });
+      window.location.replace('/StudentHome'); // student view
+    } else {
+      throw new Error('Invalid user role');
+    }
+  } catch (error) {
+    console.error(error);
+    setUser(null);
+  }
+};
+
+const App = () => {
+
+  // Check for a redirect from the OAuth2 flow
+  if (window.location.search.includes('code=')) {
+    handleAuthentication(setUser);
+  }
+
   return (
     <Router>
       <div>
@@ -26,6 +64,7 @@ function App() {
             <Route path='/ImportStudents' element={<ImportStudents/>}></Route>
             <Route path='/Students' element={<StudentTable/>}></Route>
             <Route path='/GroupView' element={<Groups/>}></Route>
+            <Route path='/StudentHome' element={<StudentHomePage/>}></Route>
             <Route path='/StudentProjects' element={<StudentProjects/>}></Route>
             <Route path='/StudentGroups' element={<StudentGroups/>}></Route>
             <Route path='/MyGroup' element={<MyGroup/>}></Route>
