3f50777a6b24a0d18e3655b40ad7c5d9ca866947
Akram-El-Gaouny <aelga098@uottawa.ca>
2023-04-04 21:31:50 -0400

    fix env

--- mongo-init.js
+++ mongo-init.js
@@ -4,7 +4,7 @@ db.students.drop();
 db.createUser(
     { 
         user: "$MONGO_INITDB_ROOT_USERNAME", 
-        pwd : "MONGO_INITDB_ROOT_PASSWORD", 
+        pwd : "$MONGO_INITDB_ROOT_PASSWORD", 
         roles: [
             {
                 role: "readWrite", 
