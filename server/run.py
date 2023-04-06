3dd914e7600dda5716d80e394db7293a2896721d
Laith-Grira <lgrira12@gmail.com>
2023-04-06 01:55:09 -0400

    call the auth

--- server/run.py
+++ server/run.py
@@ -1,19 +1,19 @@
 import os
-from flask import Flask, redirect, request, session
+from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
 from main import initialize_routes
 from uuid import uuid4
-from app.controllers import student_controller
-from app.utils.decryption_manager import getDecryptedSecret
+from security.auth import authentication
 
-
+# initilase a Flask app
 app = Flask(__name__)
 app.config["SECRET_KEY"] = str(uuid4())
 api = Api(app)
 CORS(app, resources={r"*": {"origins": "*"}})
 
-initialize_routes(app, api)
+initialize_routes(app, api) # initialise the routes to use
+authentication(app) # authenticate the user
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.getenv("FLASK"))
