f8b8d470f479425a8b3d5fcdd315f3be3cd2715b
Laith-Grira <lgrira12@gmail.com>
2023-04-07 00:15:38 -0400

    update configs

--- server/app_config.py
+++ server/app_config.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from app.utils.decryption_manager import getDecryptedSecret
 
 CLIENT_ID = getDecryptedSecret("CLIENT_ID") # Application (client) ID of app registration
@@ -13,3 +14,13 @@ ENDPOINT = 'https://graph.microsoft.com/v1.0/users'  # This resource requires no
 SCOPE = ["User.ReadBasic.All"] # permission names for the application
 
 SESSION_TYPE = "filesystem"  # Specifies the token cache should be stored in server-side session
+
+CORS_HEADERS = 'Content-Type' # Set CORS
+
+SECRET_KEY = 'ee068ec2-179b-46ca-92d1-0e3e8f138ac5' # Secret Key to uinquly identify the app
+
+PERMANENT_SESSION_LIFETIME = timedelta(hours=5) # Set lifetime of the session to 5 hours
+
+SESSION_COOKIE_SAMESITE="None" # Don't require using the same site
+
+SESSION_COOKIE_SECURE=True # Secure the cookies
