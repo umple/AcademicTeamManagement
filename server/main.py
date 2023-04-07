46fc2bd7a171537768704d22c80885beeb4d9465
Laith-Grira <lgrira12@gmail.com>
2023-04-07 00:15:59 -0400

    remove deprecated API

--- server/main.py
+++ server/main.py
@@ -1,10 +1,9 @@
-from flask_restful import Api
 from flask import Flask
 
 from app.controllers import student_controller, project_controller, group_controller
 
 
-def initialize_routes(app: Flask, api: Api):
+def initialize_routes(app: Flask):
     # routes
     app.register_blueprint(student_controller.student_bp)
     app.register_blueprint(project_controller.project_bp)
