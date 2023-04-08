f21b0b5d180d3865af7729f70cb80f7345d08ea1
Caroline Tippins <carotippins@hotmail.com>
2023-04-07 23:19:29 -0400

    Fix background cut off

--- client/src/LoginView/LoginPage.js
+++ client/src/LoginView/LoginPage.js
@@ -17,7 +17,12 @@ const Background = styled.div`
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
-  min-height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: -1;
   display: flex;
   justify-content: center;
   align-items: center;
