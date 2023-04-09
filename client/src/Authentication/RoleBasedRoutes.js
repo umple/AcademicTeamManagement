ab9a6ac175e3b7fc8d268c4bbf281ad6edaf18a7
Laith-Grira <lgrira12@gmail.com>
2023-04-09 16:47:56 -0400

    create new component for the routes

---
+++ client/src/Authentication/RoleBasedRoutes.js
@@ -0,0 +1,22 @@
+import { useState, } from 'react';
+import { Outlet, Navigate } from 'react-router-dom'
+
+const RoleBasedRoutes = ({ allowedRole }) => {
+
+  // Set the authentication variable based on the returned value of /checksession
+  const [role, setRole] = useState(true);
+
+  // Check if user exists in the session
+  fetch(`http://localhost:${process.env.REACT_APP_FLASK}/getusertype`, {
+    method: 'GET',
+    credentials: 'include' // include cookies in the request
+  })
+    .then(response => response.json())
+    .then(data => data.error ? setRole(false) : setRole(allowedRole === data.userType)); // if error exists, that means the user is not logged in yet
+    
+    return(
+        role ? <Outlet/> : <Navigate to="/NotFound"/>
+    )
+}
+
+export default RoleBasedRoutes
