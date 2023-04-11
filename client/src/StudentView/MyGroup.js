5d89e705ac8da20a4d4444896cd0ba19dd562857
Caroline Tippins <carotippins@hotmail.com>
2023-04-10 22:33:33 -0400

    My Groups improvement

--- client/src/StudentView/MyGroup.js
+++ client/src/StudentView/MyGroup.js
@@ -1,6 +1,6 @@
 // MyGroup.js
 import React, { useState, useEffect, useMemo } from "react";
-import { Box, Button, Typography, Grid, Alert, Snackbar, TableRow, TableCell } from "@mui/material";
+import { Box, Button, Typography, Grid, Alert, Snackbar, TableRow, TableCell, Card, CardContent } from "@mui/material";
 import CircularProgress from '@mui/material/CircularProgress';
 import { Link } from "react-router-dom";
 import MaterialReactTable from 'material-react-table';
@@ -87,88 +87,104 @@ const MyGroup = () => {
 
 
   return (
-
     <Box sx={{ mt: 3 }}>
       <Snackbar
         open={showAlert}
         onClose={() => setShowAlert(false)}
-        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
       >
-        <Alert severity="success">User has left the team successfully!</Alert>
+        <Alert severity="success">
+          User has left the team successfully!
+        </Alert>
       </Snackbar>
       {loading ? (
         <CircularProgress />
       ) : (
         <>
-          <Typography variant="h4" gutterBottom>
-            My Group
-          </Typography>
-          {Object.keys(group).length !== 0 ? (
-            <Box>
-              <Typography variant="h6">Group ID: {group.group_id}</Typography>
-              <Typography variant="h6">
-                Members: {group.members && group.members.join(', ')}
+          <Card sx={{ mt: 10, mb: 4, ml: 4, mr: 4 }}>
+            <CardContent>
+              <Typography variant="h5" gutterBottom>
+                My Group
               </Typography>
-              {group.project ? (
-                <Typography variant="h6">Project: {group.project}</Typography>
-              ) : (
-                <Grid container spacing={2} alignItems="center" sx={{ mt: 2 }}>
-                  <Grid item>
-                    <Link to="/StudentProjects" style={{ textDecoration: 'none' }}>
-                      <Button variant="contained" color="primary">
-                        Add Project
-                      </Button>
-                    </Link>
-                  </Grid>
-                  <Grid item>
-                    <Button
-                      variant="contained"
-                      color="error"
-                      onClick={handleLeaveGroup}
+              {Object.keys(group).length !== 0 ? (
+                <Box>
+                  <Typography sx={{ fontSize: "18px" }}>
+                    <strong>Group ID:</strong> {group.group_id} </Typography>
+                  <Typography sx={{ fontSize: "18px" }}>
+                    <strong>Members:</strong>{" "}
+                    {group.members && group.members.filter(member => member).join(", ")}
+                  </Typography>
+                  {group.project ? (
+                    <Typography sx={{ fontSize: "18px" }}>
+                      <strong>Project:</strong> {group.project}
+                    </Typography>
+                  ) : (
+                    <Grid
+                      container
+                      spacing={2}
+                      alignItems="center"
+                      sx={{ mt: 1 }}
                     >
-                      Leave Group
-                    </Button>
-                  </Grid>
-                </Grid>
-              )}
-
-              {typeof applications !== 'undefined' || applications ? (
-
-                <MaterialReactTable
-                  columns={columns}
-                  data={applications}
-                  noHeader={true}
-                  highlightOnHover={true}
-                  enableColumnFilters={false}
-                  enableHiding={false}
-                  enablePagination={false} 
-                  enableGlobalFilter={false}
-                  enableFullScreenToggle={false}
-                  enableDensityToggle={false}
-                  keyField="project"
-                  customStyles={{
-                    rows: {
-                      style: {
-                        '&:last-child td': { border: 0 },
-                      },
-                    },
-                  }}
-                />
+                      <Grid item>
+                        <Link
+                          to="/StudentProjects"
+                          style={{ textDecoration: "none" }}
+                        >
+                          <Button variant="contained" color="primary">
+                            Add Project
+                          </Button>
+                        </Link>
+                      </Grid>
+                      <Grid item>
+                        <Button
+                          variant="contained"
+                          color="error"
+                          onClick={handleLeaveGroup}
+                        >
+                          Leave Group
+                        </Button>
+                      </Grid>
+                    </Grid>
+                  )}
+                </Box>
               ) : (
-              <Typography variant="h6">
-                You have no project applications yet.
-              </Typography>
-            )}
+                <Typography variant="h6">
+                  You have not joined a group yet.
+                </Typography>
+              )}
+            </CardContent>
+          </Card>
+          {typeof applications !== "undefined" || applications ? (
+            <Box sx={{ mt: 2, ml: 4, mr:4 }}>
+              <MaterialReactTable
+                columns={columns}
+                data={applications}
+                noHeader={true}
+                highlightOnHover={true}
+                enableColumnFilters={false}
+                enableHiding={false}
+                enablePagination={false} 
+                enableGlobalFilter={false}
+                enableFullScreenToggle={false}
+                enableDensityToggle={false}
+                keyField="project"
+                customStyles={{
+                  rows: {
+                    style: {
+                      '&:last-child td': { border: 0 },
+                    },
+                  },
+                }}
+              />
             </Box>
           ) : (
-            <Typography variant="h6">You have not joined a group yet.</Typography>
+            <Typography variant="h6">
+              You have no project applications yet.
+            </Typography>
           )}
         </>
       )}
     </Box>
-
-
-
   );
 };
 
