6e2c6b273cde87e7c2fd4610c08e83ad262a8952
Akram-El-Gaouny <aelga098@uottawa.ca>
2023-07-19 10:09:41 -0400

    Remove flask-server url

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api/*',
     createProxyMiddleware({
-      target: process.env.REACT_APP_BACKEND_URL,
+      target: process.env.REACT_APP_BACKEND_HOST,
       changeOrigin: true,
     })
   );
--- server/app/utils/auth_helpers.py
+++ server/app/utils/auth_helpers.py
@@ -2,11 +2,14 @@ import os
 
 def get_redirection_url_for_user(user_role: str):
 
+    url = "http://localhost:" + os.getenv("REACT")
+    if os.getenv("BACKEND_HOST").find("localhost") == -1:
+        url = os.getenv("BACKEND_HOST")
+
     if user_role == "student":
-        redirect_url = "%s/StudentHome"%(os.getenv("BACKEND_HOST"))
+        redirect_url = "%s/StudentHome"%(url)
     elif user_role == "professor":
-        redirect_url = "%s/"%(os.getenv("BACKEND_HOST"))
+        redirect_url = "%s/"%(url)
     else:
-        redirect_url = "%s/NotFound"%(os.getenv("BACKEND_HOST")) # not implemented yet
-        
+        redirect_url = "%s/NotFound"%(url) # not implemented yet
     return redirect_url
