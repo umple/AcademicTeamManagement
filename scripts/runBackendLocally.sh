c2fb49a1305999433a88f465ab6585183e6f518f
Akram-El-Gaouny <aelga098@uottawa.ca>
2023-07-27 10:50:46 -0400

    change script

--- scripts/runBackendLocally.sh
+++ scripts/runBackendLocally.sh
@@ -6,7 +6,7 @@ export REACT=3000
 export MONGO=27017
 export FLASK=5001
 export MONGODB_INITDB_ROOT_USERNAME=root
-export MONGODB_INITDB_ROOT_PASSWORD=pass
+export MONGODB_INITDB_ROOT_PASSWORD=
 export MONGODB_HOST=localhost
 export BACKEND_HOST=http://localhost:5001
 cd server
