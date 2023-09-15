bcf5c139ba0ffb0d433d9f868572ca2abe0867a0
Robert Basile <robert.basile@mitel.com>
2023-09-15 17:49:51 -0400

    refactoring more

--- client/src/GroupView/GroupTable.js
+++ client/src/GroupView/GroupTable.js
@@ -1,5 +1,5 @@
-import React, { useCallback, useState, useMemo, useEffect } from 'react';
-import MaterialReactTable from 'material-react-table';
+import React, { useCallback, useState, useMemo, useEffect } from "react";
+import MaterialReactTable from "material-react-table";
 import {
   Box,
   Button,
@@ -17,16 +17,16 @@ import {
   cellValueMap,
   InputLabel,
   OutlinedInput,
-  Alert
-} from '@mui/material';
-import FileDownloadIcon from '@mui/icons-material/FileDownload';
-import { ExportToCsv } from 'export-to-csv'; //or use your library of choice here
-import { Delete, Edit } from '@mui/icons-material';
-import { FormControl } from '@material-ui/core';
-import Chip from '@mui/material/Chip';
-import { Theme, useTheme } from '@mui/material/styles';
-import { CreateNewGroupModal } from './CreateNewGroupModal';
-import groupsService from '../services/groupsService';
+  Alert,
+} from "@mui/material";
+import FileDownloadIcon from "@mui/icons-material/FileDownload";
+import { ExportToCsv } from "export-to-csv"; //or use your library of choice here
+import { Delete, Edit } from "@mui/icons-material";
+import { FormControl } from "@material-ui/core";
+import Chip from "@mui/material/Chip";
+import { Theme, useTheme } from "@mui/material/styles";
+import { CreateNewGroupModal } from "./CreateNewGroupModal";
+import groupsService from "../services/groupsService";
 
 const GroupTable = () => {
   const [createModalOpen, setCreateModalOpen] = useState(false);
@@ -34,7 +34,7 @@ const GroupTable = () => {
   const [validationErrors, setValidationErrors] = useState({});
   const [projects, setProjects] = useState([]);
   const [students, setStudents] = useState([]);
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     fetchGroups();
@@ -53,20 +53,22 @@ const GroupTable = () => {
   const fetchData = async () => {
     try {
       const [resProjects, resStudents] = await Promise.all([
-        fetch('/api/projects'),
-        fetch('/api/students'),
+        fetch("/api/projects"),
+        fetch("/api/students"),
       ]);
       const [projectsData, studentsData] = await Promise.all([
         resProjects.json(),
         resStudents.json(),
       ]);
 
-      if (projectsData.message !== 'Project list is empty.') {
-        const filteredProjects = projectsData.filter(project => project.status !== 'assigned');
+      if (projectsData.message !== "Project list is empty.") {
+        const filteredProjects = projectsData.filter(
+          (project) => project.status !== "assigned"
+        );
         setProjects(filteredProjects);
       }
 
-      if (studentsData.message !== 'Student list is empty.') {
+      if (studentsData.message !== "Student list is empty.") {
         setStudents(studentsData);
       }
     } catch (error) {
@@ -74,45 +76,36 @@ const GroupTable = () => {
     }
   };
 
-  const handleAddRow = useCallback(
-    async newRowData => {
+  const handleAddRow = useCallback(async (newRowData) => {
+    try {
+      let response = await groupsService.post(newRowData);
+      if (response == 200) {
+        fetchData();
+      } else if (response === 409) {
+        setMessage("The group already exists");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  const handleSaveRowEdits = async ({ exitEditingMode, row, values }) => {
+    if (!Object.keys(validationErrors).length) {
       try {
-        let response = await groupsService.post(newRowData)
-        if (response == 200) {
+        const response = await groupsService.put(row.original._id, values);
+
+        if (response === 200) {
           fetchData();
         } else if (response === 409) {
-          setMessage('The group already exists');
+          setMessage("The group already exists");
+        } else {
+          console.error("Error updating row");
         }
       } catch (error) {
         console.error(error);
       }
-    },
-    []
-  );
-
-
-  const handleSaveRowEdits = async ({ exitEditingMode, row, values }) => {
-    if (!Object.keys(validationErrors).length) {
-      fetch(`api/group/update/${row.original._id}`, {
-        method: "PUT",
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(values)
-      })
-        .then(response => {
-          if (response.ok) {
-            fetchData();
-          } else {
-            console.error("Error deleting row");
-          }
-        })
-        .catch(error => {
-          console.error(error);
-        });
       exitEditingMode();
     }
-    fetchData()
   };
 
   const handleCancelRowEdits = () => {
@@ -123,35 +116,35 @@ const GroupTable = () => {
   const handleDeleteRow = useCallback(
     (row) => {
       if (
-        !window.confirm(`Are you sure you want to delete group: ${row.getValue('group_id')}`)
+        !window.confirm(
+          `Are you sure you want to delete group: ${row.getValue("group_id")}`
+        )
       ) {
         return;
       }
-      fetch(`api/group/delete/${row.original._id}`, {
-        method: "DELETE"
-      })
-        .then(response => {
-          if (response.ok) {
-            console.log("HERE")
-            fetchData();
-            console.log(tableData)
-          } else {
-            console.error("Error deleting row");
-          }
-        })
-        .catch(error => {
-          console.error(error);
-        });
+
+      try {
+        const response = groupsService.delete(row.original._id);
+        if (response === 200) {
+          fetchData();
+        } else if (response === 409) {
+          setMessage("The group already exists");
+        } else {
+          console.error("Error updating row");
+        }
+      } catch (error) {
+        console.error(error);
+      }
     },
-    [tableData],
+    [tableData]
   );
 
   // For exporting the table data
   const csvOptions = {
-    filename: 'GroupsFromAcTeams-' + getDate(),
-    fieldSeparator: ',',
+    filename: "GroupsFromAcTeams-" + getDate(),
+    fieldSeparator: ",",
     quoteStrings: '"',
-    decimalSeparator: '.',
+    decimalSeparator: ".",
     showLabels: true,
     useBom: true,
     useKeysAsHeaders: true,
@@ -161,32 +154,32 @@ const GroupTable = () => {
 
   const handleExportData = () => {
     // clean up and organize data to be exported
-    const keyToRemove = "_id"
-    const updatedJsonList = tableData.map(jsonObj => {
-      let updatedJsonObject = jsonObj
+    const keyToRemove = "_id";
+    const updatedJsonList = tableData.map((jsonObj) => {
+      let updatedJsonObject = jsonObj;
       // remove the _id as that should not be in the json
       if (keyToRemove in jsonObj) {
-        const { [keyToRemove]: deletedKey, ...rest } = jsonObj // use destructuring to remove the key
-        updatedJsonObject = rest // return the updated JSON object without the deleted key
+        const { [keyToRemove]: deletedKey, ...rest } = jsonObj; // use destructuring to remove the key
+        updatedJsonObject = rest; // return the updated JSON object without the deleted key
       }
 
       // sort the keys as they appear in the columns
-      const orderedKeys = columns.map(key => key.accessorKey)
+      const orderedKeys = columns.map((key) => key.accessorKey);
       updatedJsonObject = Object.keys(updatedJsonObject)
         .sort((a, b) => orderedKeys.indexOf(a) - orderedKeys.indexOf(b)) // sort keys in the order of the updated keys
-        .reduce((acc, key) => ({ ...acc, [key]: updatedJsonObject[key] }), {}) // create a new object with sorted keys
+        .reduce((acc, key) => ({ ...acc, [key]: updatedJsonObject[key] }), {}); // create a new object with sorted keys
 
       // replace the accessor key by the header
       for (let i = 0; i < columns.length; i++) {
-        const { accessorKey, header } = columns[i]
+        const { accessorKey, header } = columns[i];
         if (accessorKey in updatedJsonObject) {
-          const { [accessorKey]: renamedKey, ...rest } = updatedJsonObject // use destructuring to rename the key
-          updatedJsonObject = { ...rest, [header]: renamedKey } // update the JSON object with the renamed key
+          const { [accessorKey]: renamedKey, ...rest } = updatedJsonObject; // use destructuring to rename the key
+          updatedJsonObject = { ...rest, [header]: renamedKey }; // update the JSON object with the renamed key
         }
       }
 
-      return updatedJsonObject // return the original JSON object if the key is not found
-    })
+      return updatedJsonObject; // return the original JSON object if the key is not found
+    });
 
     csvExporter.generateCsv(updatedJsonList);
   };
@@ -194,21 +187,28 @@ const GroupTable = () => {
   function getDate() {
     const today = new Date();
     const year = today.getFullYear();
-    const month = String(today.getMonth() + 1).padStart(2, '0');
-    const day = String(today.getDate()).padStart(2, '0');
+    const month = String(today.getMonth() + 1).padStart(2, "0");
+    const day = String(today.getDate()).padStart(2, "0");
     const formattedDate = `${year}-${month}-${day}`;
-    return formattedDate
+    return formattedDate;
   }
 
   return (
     <Box sx={{ p: 2 }}>
-      <Typography variant="h2" align="center" fontWeight="fontWeightBold" sx={{ marginBottom: '0.5rem' }}>Groups</Typography>
+      <Typography
+        variant="h2"
+        align="center"
+        fontWeight="fontWeightBold"
+        sx={{ marginBottom: "0.5rem" }}
+      >
+        Groups
+      </Typography>
       {message === "" ? "" : <Alert severity="warning">{message}</Alert>}
       <MaterialReactTable
         displayColumnDefOptions={{
-          'mrt-row-actions': {
+          "mrt-row-actions": {
             muiTableHeadCellProps: {
-              align: 'center',
+              align: "center",
             },
             size: 120,
           },
@@ -225,11 +225,11 @@ const GroupTable = () => {
           size: 150, //default size is usually 180
         }}
         enableEditing
-        initialState={{ showColumnFilters: false, density: 'compact' }}
+        initialState={{ showColumnFilters: false, density: "compact" }}
         onEditingRowSave={handleSaveRowEdits}
         onEditingRowCancel={handleCancelRowEdits}
         renderRowActions={({ row, table }) => (
-          <Box sx={{ display: 'flex', gap: '1rem' }}>
+          <Box sx={{ display: "flex", gap: "1rem" }}>
             <Tooltip arrow placement="left" title="Edit">
               <IconButton onClick={() => table.setEditingRow(row)}>
                 <Edit />
@@ -243,7 +243,9 @@ const GroupTable = () => {
           </Box>
         )}
         renderTopToolbarCustomActions={() => (
-          <Box sx={{ display: 'flex', gap: '1rem', p: '0.5rem', flexWrap: 'wrap' }}>
+          <Box
+            sx={{ display: "flex", gap: "1rem", p: "0.5rem", flexWrap: "wrap" }}
+          >
             <Button
               color="success"
               onClick={() => setCreateModalOpen(true)}
@@ -276,6 +278,5 @@ const GroupTable = () => {
     </Box>
   );
 };
-
 
 export default GroupTable;
--- client/src/services/groupsService.js
+++ client/src/services/groupsService.js
@@ -1,32 +1,63 @@
-import axios from "axios"
+import axios from "axios";
 const groupsService = {
-    get: async () => {
-        return await axios.get("/api/groups").then((response) => { 
-            return response.json() 
-        }).then((data) => {
-            return data;
-        }).catch((error) => { return error });
-    },
-    post: async (values) => {
-        try {
-            const response = await fetch("api/group", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(values)
-            });
+  get: async () => {
+    return await axios
+      .get("/api/groups")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        return data;
+      })
+      .catch((error) => {
+        return error;
+      });
+  },
+  post: async (values) => {
+    try {
+      const response = await fetch("api/group", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      });
 
-            if (response.ok) {
-                return response.status;
-            } else {
-                throw new Error(`Request failed with status: ${response.status}`);
-            }
-        } catch (error) {
-            console.error(error);
-        }
+      if (response.ok) {
+        return response.status;
+      } else {
+        throw new Error(`Request failed with status: ${response.status}`);
+      }
+    } catch (error) {
+      console.error(error);
     }
-
-}
+  },
+  put: async (groupId, values) => {
+    return fetch(`api/group/update/${groupId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(values),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .catch((error) => {
+        return error;
+      });
+  },
+  delete: async (id) => {
+    return fetch(`api/group/delete/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .catch((error) => {
+        return error;
+      });
+  },
+};
 
 export default groupsService;
