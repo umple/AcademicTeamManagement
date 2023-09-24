7c0a7e9d2031d93a867c8aafdf75a058e2c8fe43
Robert Basile <robert.basile@mitel.com>
2023-09-23 20:32:37 -0400

    fixed formatting

--- client/src/App.js
+++ client/src/App.js
@@ -18,8 +18,8 @@ import StudentHomePage from "./pages/StudentHomePage";
 const ROLES = {
   PROFESSOR: "professor",
   STUDENT: "student",
-  NONE: ""
-}
+  NONE: "",
+};
 
 const App = () => {
   return (
@@ -31,38 +31,21 @@ const App = () => {
           </div>
           <Routes>
             <Route element={<PrivateRoutes />}>
-              <Route
-                element={<RoleBasedRoutes allowedRole={ROLES.PROFESSOR} />}
-              >
+              <Route element={<RoleBasedRoutes allowedRole={ROLES.PROFESSOR} />}>
                 <Route path="/ProfessorHome" element={<HomePage />}></Route>
                 <Route path="/Students" element={<StudentTable />}></Route>
-                <Route
-                  exact
-                  path="/Projects"
-                  element={<ProjectTable />}
-                ></Route>
-                <Route
-                  path="/ImportStudents"
-                  element={<ImportStudents />}
-                ></Route>
+                <Route exact path="/Projects" element={<ProjectTable />}></Route>
+                <Route path="/ImportStudents" element={<ImportStudents />}></Route>
                 <Route path="/GroupView" element={<Groups />}></Route>
               </Route>
 
               <Route element={<RoleBasedRoutes allowedRole={ROLES.STUDENT} />}>
-                <Route
-                  path="/StudentHome"
-                  element={<StudentHomePage />}
-                ></Route>
-                <Route
-                  path="/StudentProjects"
-                  element={<StudentProjects />}
-                ></Route>
-                <Route
-                  path="/StudentGroups"
-                  element={<StudentGroups />}
-                ></Route>
+                <Route path="/StudentHome" element={<StudentHomePage />}></Route>
+                <Route path="/StudentProjects" element={<StudentProjects />}></Route>
+                <Route path="/StudentGroups"  element={<StudentGroups />}></Route>
                 <Route path="/MyGroup" element={<MyGroup />}></Route>
               </Route>
+              
             </Route>
             <Route path="/" element={<LoginPage />}></Route>
             <Route path="/login" element={<LoginPage />}></Route>
