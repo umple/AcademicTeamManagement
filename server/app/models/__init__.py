3c9c9611e95a3a246a2adc89557128b4a621e841
Akram-El-Gaouny <aelga098@uottawa.ca>
2023-09-28 18:11:40 -0400

    Fix mongo issue

--- server/app/models/__init__.py
+++ server/app/models/__init__.py
@@ -6,8 +6,15 @@ import mongomock
 PORT = int(os.getenv("MONGO")) if os.getenv("MONGO") else 27017
 
 # Initialize clients
-client = None
-db = None
+client = MongoClient(
+    host=os.getenv("MONGODB_HOST"),
+    port=PORT,
+    username=os.getenv("MONGODB_INITDB_ROOT_USERNAME"),
+    password=os.getenv("MONGODB_INITDB_ROOT_PASSWORD"),
+    authSource="AcademicTeamManagementDB"
+)   
+# Main database
+db = client["AcademicTeamManagementDB"]
 
 # Check if a testing environment is running
 if os.getenv('ENVIRONMENT') == 'TESTING':
@@ -15,13 +22,5 @@ if os.getenv('ENVIRONMENT') == 'TESTING':
     print("In a Test Environment")
     client = mongomock.MongoClient()
     db = client.db
-else:
-    client = MongoClient(
-        host=os.getenv("MONGODB_HOST"),
-        port=PORT,
-        username=os.getenv("MONGODB_INITDB_ROOT_USERNAME"),
-        password=os.getenv("MONGODB_INITDB_ROOT_PASSWORD"),
-        authSource="AcademicTeamManagementDB"
-    )   
-    # Main database
-    db = client["AcademicTeamManagementDB"]
+
+
