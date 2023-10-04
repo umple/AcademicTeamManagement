f25cd6ed959df172cdc5a2d1ed2e1309ad55646c
Laith-Grira <lgrira12@gmail.com>
2023-10-04 14:59:22 -0400

    adjust the group ID

--- server/tests/pipeline_tests/unit/test_group_model.py
+++ server/tests/pipeline_tests/unit/test_group_model.py
@@ -123,7 +123,7 @@ class TestGroupModification(unittest.TestCase):
     def test_remove_student_from_group_by_email(self, mock_get_student_by_email):
         # Mock the behavior of get_student_by_email
         mock_get_student_by_email.return_value = {"orgdefinedid": "12345", "email": "test@example.com"}
-        actual = group.remove_student_from_group_by_email(self.group["group_id"], "test@example.com")
+        actual = group.remove_student_from_group_by_email(self.group["_id"], "test@example.com")
         self.assertTrue(actual)
 
         # Validate member is removed
@@ -135,9 +135,9 @@ class TestGroupModification(unittest.TestCase):
         # Mock the behavior of get_student_by_email
         mock_get_student_by_email.return_value = {"orgdefinedid": "12345", "email": "test@example.com"}
         # Add student to the group
-        group.add_student_to_group("test@example.com", self.group["group_id"])
+        group.add_student_to_group("test@example.com", self.group["_id"])
         # Remove student from the group
-        actual = group.remove_student_from_group(self.group["group_id"], "12345")
+        actual = group.remove_student_from_group(self.group["_id"], "12345")
         self.assertTrue(actual)
 
         # Validate member is removed
