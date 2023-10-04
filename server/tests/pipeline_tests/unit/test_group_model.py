54206d0925bf2be1bd64f8b5cdf9cae8062db037
Laith-Grira <lgrira12@gmail.com>
2023-10-04 14:39:01 -0400

    fix members added

--- server/tests/pipeline_tests/unit/test_group_model.py
+++ server/tests/pipeline_tests/unit/test_group_model.py
@@ -117,7 +117,7 @@ class TestGroupModification(unittest.TestCase):
 
         # Validate member is added
         actualMembers = group.get_group(self.group["_id"])["members"]
-        self.assertTrue("test@example.com" in actualMembers)
+        self.assertTrue("test@example.com" == actualMembers[0]["email"])
 
     def test_remove_student_from_group_by_email(self):
         actual = group.remove_student_from_group_by_email(self.group["group_id"], "test@example.com")
