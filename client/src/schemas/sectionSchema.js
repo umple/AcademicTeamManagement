4a56c2e39b5f4f4837e9b010f83fb91f351db55d
Laith-Grira <lgrira12@gmail.com>
2023-11-03 16:34:17 -0400

    validation improvment

--- client/src/schemas/sectionSchema.js
+++ client/src/schemas/sectionSchema.js
@@ -4,14 +4,13 @@ const sectionSchema =  (sections,_id) => {
   
   const sectionSchema = Yup.object().shape({
   name: Yup.string().required("Name is required").test("is-unique", "Name already exists", function (value) {
-    const group = sections.find(
-      (section) => section._name != _id && section.name === value
+    const section = sections.find(
+      (section) => section._id != _id && section.name === value 
     );
-    return typeof group === "undefined";
+    return typeof section === "undefined";
   }),
   term: Yup.string().required("Term is required"),
   year: Yup.string().required("Year is required"),
-  notes: Yup.string().required("Notes is required"),
 });
   return sectionSchema
 }
