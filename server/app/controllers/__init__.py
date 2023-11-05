7b4aad97554c28e8e4e58c87e23f6a562b3f3b18
Laith-Grira <lgrira12@gmail.com>
2023-11-04 21:49:29 -0400

    add bluprints and apis

--- server/app/controllers/__init__.py
+++ server/app/controllers/__init__.py
@@ -2,9 +2,11 @@ from flask import Blueprint
 
 student_bp = Blueprint('student', __name__,url_prefix='/api')
 project_bp = Blueprint('project',__name__,url_prefix='/api')
+user_bp = Blueprint('user',__name__,url_prefix='/api')
+staff_bp = Blueprint('staff',__name__,url_prefix='/api')
 group_bp = Blueprint('group',__name__,url_prefix='/api')
 section_bp = Blueprint('section', __name__, url_prefix='/api')
 project_application_bp = Blueprint('projectApplication',__name__,url_prefix='/api')
 
 
-from . import student_controller, project_controller, group_controller, section_controller, project_application_controller
+from . import student_controller, project_controller, group_controller, section_controller, user_controller, staff_controller, project_application_controller
--- server/main.py
+++ server/main.py
@@ -1,11 +1,13 @@
 from flask import Flask
 
-from app.controllers import student_controller, section_controller, project_controller, group_controller, project_application_controller
+from app.controllers import student_controller, user_controller, staff_controller, section_controller, project_controller, group_controller, project_application_controller
 
 
 def initialize_routes(app: Flask):
     # routes
     app.register_blueprint(student_controller.student_bp)
+    app.register_blueprint(user_controller.user_bp)
+    app.register_blueprint(staff_controller.staff_bp)
     app.register_blueprint(project_controller.project_bp)
     app.register_blueprint(section_controller.section_bp)
     app.register_blueprint(group_controller.group_bp)
