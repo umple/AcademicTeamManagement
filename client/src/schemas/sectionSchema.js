e033e44189721a225cee61fa78fa99841d8701a1
Laith-Grira <lgrira12@gmail.com>
2023-11-13 11:13:11 -0500

    update section schema

--- client/src/schemas/sectionSchema.js
+++ client/src/schemas/sectionSchema.js
@@ -4,10 +4,14 @@ const sectionSchema =  (sections,_id) => {
   
   const sectionSchema = Yup.object().shape({
   name: Yup.string().required("Name is required").test("is-unique", "Name already exists", function (value) {
-    const section = sections.find(
-      (section) => section._id != _id && section.name === value 
-    );
-    return typeof section === "undefined";
+    if (sections && sections.length > 0) {
+      const section = sections.find(
+        (section) => section._id != _id && section.name === value 
+      );
+      return typeof section === "undefined";
+    } else {
+      return true
+    }
   }),
   term: Yup.string().required("Term is required"),
   year: Yup.string().required("Year is required"),
