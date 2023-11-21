e11fe2f33869aa4d1db578877973b494ead7f129
andre1048576 <55166043+andre1048576@users.noreply.github.com>
2023-11-21 14:44:30 -0500

    Update GroupTable.js

--- client/src/components/professorComponents/tables/GroupTable.js
+++ client/src/components/professorComponents/tables/GroupTable.js
@@ -265,8 +265,7 @@ const GroupTable = () => {
           size: 150, //default size is usually 180
         }}
         enableEditing
-        initialState={{ showColumnFilters: false, showGlobalFilter: true, density: 'compact' ,pagination: {pageSize:1000}}}
-        localization={tableLocalization}
+        initialState={{ showColumnFilters: false, density: "compact",pagination: {pageSize:200}}}
         renderRowActions={({ row, table }) => (
           <Box sx={{ display: "flex", gap: "1rem" }} id={`row-${row.original.group_id}`}>
             <Tooltip arrow placement="left" title="Edit">
