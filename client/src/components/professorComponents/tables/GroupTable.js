264a3798ccc873ac20e2aa0bafd71611cbc9ce7a
Laith-Grira <lgrira12@gmail.com>
2023-12-01 10:37:38 -0500

    update the group table

--- client/src/components/professorComponents/tables/GroupTable.js
+++ client/src/components/professorComponents/tables/GroupTable.js
@@ -130,7 +130,35 @@ const GroupTable = () => {
       },
       {
         accessorKey: 'project',
-        header: t('common.Project')
+        header: t('common.Project'),
+        Cell: ({ cell }) => {
+          if (cell.getValue('project').length > 0) {
+            return (
+              <div>
+                <Chip sx = {{ marginBottom: '5px' }} color="secondary" label={cell.getValue('project')}/>
+              </div>
+              )
+          } else {
+            return null
+          }
+        }
+      },
+      {
+        accessorKey: 'interest',
+        header: t('common.interested-projects'),
+        Cell: ({ cell }) => {
+          if (Array.isArray(cell.getValue('interest')) && cell.getValue('interest').length > 0) {
+              return cell.getValue('interest').map((value, index) => {
+                return (
+                  <div>
+                    <Chip sx = {{ marginBottom: '5px' }} color="primary" label={value}/>
+                  </div>
+                  )
+              })
+          } else {
+            return null
+          }
+        }
       },
       {
         accessorKey: 'notes',
