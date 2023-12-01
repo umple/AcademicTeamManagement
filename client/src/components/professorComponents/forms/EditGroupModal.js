e8ed605ffbd2d7188d8e2b8832d7479fcb2d439e
Laith-Grira <lgrira12@gmail.com>
2023-12-01 10:37:20 -0500

    update the edit modal

--- client/src/components/professorComponents/forms/EditGroupModal.js
+++ client/src/components/professorComponents/forms/EditGroupModal.js
@@ -239,6 +239,10 @@ const EditGroupModal = ({
                 )
               }
 
+              if (column.accessorKey === 'interest') {
+                return null
+              }
+
               return (
                 <TextField
                   key={column.accessorKey}
