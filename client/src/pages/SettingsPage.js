b1772965f2132b048c8ed148a21b5ce801e41709
Elizabeth Primeau <ella.j.primeau@gmail.com>
2024-02-28 14:04:26 -0500

    implemented export function

--- client/src/pages/SettingsPage.js
+++ client/src/pages/SettingsPage.js
@@ -1,15 +1,18 @@
 import * as React from 'react'
-import { Button, Link, Box, Container, CssBaseline } from '@mui/material'
+import { Button, Box, Container, CssBaseline } from '@mui/material'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 import styled from 'styled-components'
 import FileDownloadIcon from '@mui/icons-material/FileDownload'
-import { handleGlobalExportData } from '../helpers/exportData'
+import { ExportToCsv } from 'export-to-csv'
+import { csvOptions, handleGlobalExportData } from '../helpers/exportData'
 
 const theme = createTheme()
+const csvExporter = new ExportToCsv(csvOptions('AcademicTeamsExport-'))
 
 const WhiteContainer = styled.div`
+  margin-top: 50px;
   background-color: white;
-  padding: 40px;
+  padding: 0 40px 40px 40px;
   border-radius: 5px;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 `
@@ -17,35 +20,25 @@ const WhiteContainer = styled.div`
 export default function SettingsPage () {
   return (
     <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
+      <Container component="main" maxWidth="md">
         <CssBaseline />
         <WhiteContainer>
           <Box
             sx={{
               display: 'flex',
               flexDirection: 'column',
-              alignItems: 'center'
+              alignItems: 'auto'
             }}
           >
+            <h2 style={{ Align: 'left' }}>Data protection</h2>
             <Button
               color="primary"
-              onClick={() => handleGlobalExportData()}
+              onClick={() => handleGlobalExportData(csvExporter)}
               startIcon={<FileDownloadIcon />}
               variant="contained"
             >
-              Export
+              Export data as JSON
             </Button>
-            <Link
-              href={`${process.env.REACT_APP_BACKEND_HOST}/api/login`}
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              <Button size="large" fullWidth variant="contained" name="login" color="primary">
-                Export
-              </Button>
-            </Link>
           </Box>
         </WhiteContainer>
       </Container>
