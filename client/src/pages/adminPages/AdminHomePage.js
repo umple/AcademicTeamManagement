f2511af2462abb6f7f974c9d519be9fd6732a264
tye101 <tye101@uottawa.ca>
2024-03-06 21:23:33 -0500

    add footer to admin homepage

--- client/src/pages/adminPages/AdminHomePage.js
+++ client/src/pages/adminPages/AdminHomePage.js
@@ -17,6 +17,22 @@ import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
+const FooterContainer = styled.footer`
+  background-color: #2c3e50;
+  color: #ecf0f1;
+  display: flex; // Use flexbox for layout
+  justify-content: center; // Center content horizontally
+  align-items: center; // Align items vertically
+  padding: 20px;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  flex-wrap: nowrap; // Prevent wrapping by default
+  font-family: 'Helvetica', 'Arial', sans-serif;
+  white-space: nowrap; // Prevent wrapping at the whitespace in the content
+  overflow-x: auto; // Allow horizontal scrolling if content is too wide
+`
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
