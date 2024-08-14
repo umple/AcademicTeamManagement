b390424f8b628b812074c698df68473b7e38ecf6
Elizabeth Primeau <ella.j.primeau@gmail.com>
2024-08-14 11:20:37 -0400

    rebased onto master

--- scripts/runAll.sh
+++ scripts/runAll.sh
@@ -1,4 +1,10 @@
 systemctl start docker
 kill -9 $(lsof -i:5001 -t) 2> /dev/null
 kill -9 $(lsof -i:3000 -t) 2> /dev/null
+<<<<<<< HEAD
 sudo gradle startMongo & sh scripts/runBackendLocally.sh & sudo gradle runFrontendLocally && fg
+=======
+ls
+chmod +x ./gradlew
+sudo ./gradlew startMongo & sudo ./gradlew runBackendLocally & sudo ./gradlew runFrontendLocally && fg
+>>>>>>> dbac6e0 (rebased onto master)
