e3c6eeefe25ef2fbc11c43088327bebd088b0669
Elizabeth Primeau <ella.j.primeau@gmail.com>
2024-08-14 11:03:31 -0400

    finished restructuring entity files

--- scripts/runAll.sh
+++
@@ -1,6 +0,0 @@
-systemctl start docker
-kill -9 $(lsof -i:5001 -t) 2> /dev/null
-kill -9 $(lsof -i:3000 -t) 2> /dev/null
-ls
-chmod +x ./gradlew
-sudo ./gradlew startMongo & sudo ./gradlew runBackendLocally & sudo ./gradlew runFrontendLocally && fg
